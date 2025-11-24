4242b60299e7bce9e482fbadf388912be0b76065
AndRomero1492 <alvaroa.romeror@ecci.edu.co>
2025-11-24 18:45:55 -0500

    Refactor clean_text function for clarity

--- smav_service/preprocess.py
+++ smav_service/preprocess.py
@@ -2,19 +2,19 @@ import re
 import unicodedata
 
 def clean_text(text):
-    # Minúsculas
+
     text = text.lower()
 
-    # Quitar acentos
+
     text = ''.join(
         c for c in unicodedata.normalize('NFD', text)
         if unicodedata.category(c) != 'Mn'
     )
 
-    # Quitar caracteres raros
+
     text = re.sub(r"[^a-zA-Z0-9áéíóúñü\s]", " ", text)
 
-    # Quitar espacios múltiples
+
     text = re.sub(r"\s+", " ", text)
 
     return text.strip()
